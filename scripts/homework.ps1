ed275b6bce6b688ba0ce91d6a3023d69b49d677f
Arthur Martelli <66151338+ArthurMartelli@users.noreply.github.com>
2022-07-21 17:10:53 -0600

    Changed Homework script

--- scripts/homework.ps1
+++ scripts/homework.ps1
@@ -1,1 +1,7 @@
-Invoke-Expression "python $HOME/github/homework"
+$DIR = "$HOME/github/homework"
+
+Set-Location $DIR
+Invoke-Expression "gh repo sync"
+Invoke-Expression "python $DIR"
+
+Set-Location $HOME
